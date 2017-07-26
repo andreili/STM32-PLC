0659073d39f1863a32c1df0660af21833d023d45
andreili <admin@andreil.by>
2017-07-26 14:57:31 +0300

    Добавлена инициализация FPU

--- firmware/fw_cpu/src/init.cpp
+++ firmware/fw_cpu/src/init.cpp
@@ -13,6 +13,10 @@ int main();
 // base initialization
 void base_init()
 {
+    /* FPU settings ------------------------------------------------------------*/
+    #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
+    SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
+    #endif
     STM32_RCC::deinit();
     STM32_GPIO::init_all();
     STM32_SYSTICK::init();
